e1ac9437d5d6ad468e0831c70e776a83e1120f89
halagharaibeh21 <124539532+halagharaibeh21@users.noreply.github.com>
2024-12-24 17:38:45 +0300

    Update bookimages.dart

--- lib/bookimages.dart
+++ lib/bookimages.dart
@@ -7,8 +7,8 @@ class PrideandPrejudice_pic extends StatelessWidget {
       padding: EdgeInsets.all(8.0), 
       child: Image.network(
         "https://d28hgpri8am2if.cloudfront.net/book_images/onix/cvr9781524861759/pride-and-prejudice-9781524861759_hr.jpg",
-        width: 75, 
-        height: 75, 
+        width: 80, 
+        height: 85, 
         fit: BoxFit.cover, 
       ), );}}
 
@@ -19,11 +19,10 @@ class MobyDick_pic extends StatelessWidget {
       padding: EdgeInsets.all(8.0), // Define padding
       child: Image.network(
         "https://m.media-amazon.com/images/I/616R20nvohL._AC_UF1000,1000_QL80_.jpg",
-        width: 75, // Increased width
-        height: 75, // Increased height
+        width: 80, // Increased width
+        height: 85, // Increased height
         fit: BoxFit.cover, // Ensures the image fits well
       ), );}}
-
 class Frankenstein_pic extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -31,8 +30,8 @@ class Frankenstein_pic extends StatelessWidget {
       padding: EdgeInsets.all(8.0), // Define padding
       child: Image.network(
         "https://images.saymedia-content.com/.image/t_share/MTg2Nzg5MjA1NDcxNDcxMDg3/by-its-cover-frankenstein-by-mary-shelley.jpg",
-        width: 75, // Increased width
-        height: 75, // Increased height
+        width: 80, // Increased width
+        height: 85, // Increased height
         fit: BoxFit.cover, // Ensures the image fits well
       ), );}}
 
@@ -45,8 +44,8 @@ class TheGreatGatsby_pic extends StatelessWidget {
       padding: EdgeInsets.all(8.0), // Define padding
       child: Image.network(
         "https://d28hgpri8am2if.cloudfront.net/book_images/onix/cvr9781524879761/the-great-gatsby-9781524879761_hr.jpg",
-        width: 75, // Increased width
-        height: 75, // Increased height
+        width: 80, // Increased width
+        height: 85, // Increased height
         fit: BoxFit.cover, // Ensures the image fits well
       ), );}}
 
@@ -57,8 +56,8 @@ class AliceInWonderland_pic extends StatelessWidget {
       padding: EdgeInsets.all(8.0), // Define padding
       child: Image.network(
         "https://ik.imagekit.io/panmac/tr:di-placeholder_portrait_aMjPtD9YZ.jpg,tr:w-350,f-jpg,pr-true/edition/9781447279990.jpg",
-        width: 75, // Increased width
-        height: 75, // Increased height
+        width: 80, // Increased width
+        height: 85, // Increased height
         fit: BoxFit.cover, // Ensures the image fits well
       ), );}}
 
@@ -69,7 +68,7 @@ class AliceInWonderland_pic extends StatelessWidget {
       padding: EdgeInsets.all(8.0), // Define padding
       child: Image.network(
         "https://lh3.googleusercontent.com/proxy/Xe2gaEJVio2KOW2qYObb6pMfZwwluEuBhkwlnbuwEPZRc8FZmjm2o0A8OQ526Vp56bMUo96VMVM6AbKTlaZFzW4qndGOwIITlXtexWJo78gnbAOsSZbLshG5",
-        width: 75, // Increased width
-        height: 75, // Increased height
-        fit: BoxFit.cover, // Ensures the image fits well
+        width: 80, // Increased width
+        height: 85, // Increased height
+        fit: BoxFit.fitHeight, 
       ), );}}
