49b0c0f47fdd70043a54c9668465b59e629dddbf
halagharaibeh21 <124539532+halagharaibeh21@users.noreply.github.com>
2024-12-24 20:50:17 +0300

    Update book_G_E.dart

--- lib/book_G_E.dart
+++ lib/book_G_E.dart
@@ -11,24 +11,43 @@ class PrideandPrejudice extends StatelessWidget {
     return Column(
       children: [
         PrideandPrejudice_pic(),
-        Text(bookBox.get('K_PrideAndPrejudice')!.genre,
+        Text(bookBox.get('K_PrideAndPrejudice')!.genre + " - " + bookBox.get('K_PrideAndPrejudice')!.price.toString() + "\$",
             style: GoogleFonts.oswald(
               color: const Color.fromARGB(255, 45, 41, 41),
               fontSize: 12,
             )),
-        ElevatedButton(
-          onPressed: () {},
-          child: Text(
-            'More information',
-            style: TextStyle(color: Colors.black), // Font color changed to black
-          ),
-          style: ElevatedButton.styleFrom(
-            textStyle: TextStyle(fontSize: 10),
-            backgroundColor: Colors.grey, // Grayish color
-            padding: EdgeInsets.symmetric(vertical: 16, horizontal: 16), // Smaller button padding
-            minimumSize: Size(0, 0), // Smaller button size
-          ),
-        ),
+         Row(
+  mainAxisAlignment: MainAxisAlignment.center, // Center the buttons
+  children: [
+    ElevatedButton(
+      onPressed: () {},
+      child: Text(
+        'More',
+        style: TextStyle(color: Colors.black), // Font color changed to black
+      ),
+      style: ElevatedButton.styleFrom(
+        textStyle: TextStyle(fontSize: 10),
+        backgroundColor: Color.fromARGB(255, 124, 142, 197), // Grayish color
+        padding: EdgeInsets.symmetric(vertical: 15, horizontal: 25), // Smaller button padding
+        minimumSize: Size(0, 0),
+      ),
+    ), 
+    SizedBox(width: 8), // Add some spacing between the buttons
+    ElevatedButton(
+      onPressed: () {},
+      child: Text(
+        'Buy',
+        style: TextStyle(color: Colors.black), // Font color changed to black
+      ),
+      style: ElevatedButton.styleFrom(
+        textStyle: TextStyle(fontSize: 10),
+        backgroundColor: Color.fromARGB(255, 124, 142, 197), // Grayish color
+        padding: EdgeInsets.symmetric(vertical: 15, horizontal: 25), // Smaller button padding
+        minimumSize: Size(0, 0),
+      ),
+    ),
+  ],
+),
       ],
     );
   }
@@ -41,24 +60,43 @@ class MobyDick extends StatelessWidget {
     return Column(
       children: [
         MobyDick_pic(),
-        Text(bookBox.get('K_MobyDick')!.genre,
+        Text(bookBox.get('K_MobyDick')!.genre + " - " + bookBox.get('K_MobyDick')!.price.toString() + "\$",
             style: GoogleFonts.oswald(
               color: const Color.fromARGB(255, 45, 41, 41),
               fontSize: 12,
             )),
-        ElevatedButton(
-          onPressed: () {},
-          child: Text(
-            'More information',
-            style: TextStyle(color: Colors.black), // Font color changed to black
-          ),
-          style: ElevatedButton.styleFrom(
-            textStyle: TextStyle(fontSize: 10),
-            backgroundColor: Colors.grey, // Grayish color
-            padding: EdgeInsets.symmetric(vertical: 16, horizontal: 16), // Smaller button padding
-            minimumSize: Size(0, 0), // Smaller button size
-          ),
-        ),
+       Row(
+  mainAxisAlignment: MainAxisAlignment.center, // Center the buttons
+  children: [
+    ElevatedButton(
+      onPressed: () {},
+      child: Text(
+        'More',
+        style: TextStyle(color: Colors.black), // Font color changed to black
+      ),
+      style: ElevatedButton.styleFrom(
+        textStyle: TextStyle(fontSize: 10),
+        backgroundColor: Color.fromARGB(255, 124, 142, 197), // Grayish color
+        padding: EdgeInsets.symmetric(vertical: 15, horizontal: 25), // Smaller button padding
+        minimumSize: Size(0, 0),
+      ),
+    ), 
+    SizedBox(width: 8), // Add some spacing between the buttons
+    ElevatedButton(
+      onPressed: () {},
+      child: Text(
+        'Buy',
+        style: TextStyle(color: Colors.black), // Font color changed to black
+      ),
+      style: ElevatedButton.styleFrom(
+        textStyle: TextStyle(fontSize: 10),
+        backgroundColor: Color.fromARGB(255, 124, 142, 197), // Grayish color
+        padding: EdgeInsets.symmetric(vertical: 15, horizontal: 25), // Smaller button padding
+        minimumSize: Size(0, 0),
+      ),
+    ),
+  ],
+),
       ],
     );
   }
@@ -71,24 +109,43 @@ class Frankenstein extends StatelessWidget {
     return Column(
       children: [
         Frankenstein_pic(),
-        Text(bookBox.get('K_Frankenstein')!.genre,
+        Text(bookBox.get('K_Frankenstein')!.genre + " - " + bookBox.get('K_Frankenstein')!.price.toString() + "\$",
             style: GoogleFonts.oswald(
               color: const Color.fromARGB(255, 45, 41, 41),
               fontSize: 12,
             )),
-        ElevatedButton(
-          onPressed: () {},
-          child: Text(
-            'More information',
-            style: TextStyle(color: Colors.black), // Font color changed to black
-          ),
-          style: ElevatedButton.styleFrom(
-            textStyle: TextStyle(fontSize: 10),
-            backgroundColor: Colors.grey, // Grayish color
-            padding: EdgeInsets.symmetric(vertical: 16, horizontal: 16), // Smaller button padding
-            minimumSize: Size(0, 0), // Smaller button size
-          ),
-        ),
+        Row(
+  mainAxisAlignment: MainAxisAlignment.center, // Center the buttons
+  children: [
+    ElevatedButton(
+      onPressed: () {},
+      child: Text(
+        'More',
+        style: TextStyle(color: Colors.black), // Font color changed to black
+      ),
+      style: ElevatedButton.styleFrom(
+        textStyle: TextStyle(fontSize: 10),
+        backgroundColor: Color.fromARGB(255, 124, 142, 197), // Grayish color
+        padding: EdgeInsets.symmetric(vertical: 15, horizontal: 25), // Smaller button padding
+        minimumSize: Size(0, 0),
+      ),
+    ), 
+    SizedBox(width: 8), // Add some spacing between the buttons
+    ElevatedButton(
+      onPressed: () {},
+      child: Text(
+        'Buy',
+        style: TextStyle(color: Colors.black), // Font color changed to black
+      ),
+      style: ElevatedButton.styleFrom(
+        textStyle: TextStyle(fontSize: 10),
+        backgroundColor: Color.fromARGB(255, 124, 142, 197), // Grayish color
+        padding: EdgeInsets.symmetric(vertical: 15, horizontal: 25), // Smaller button padding
+        minimumSize: Size(0, 0),
+      ),
+    ),
+  ],
+),
       ],
     );
   }
@@ -101,24 +158,50 @@ class TheGreatGatsby extends StatelessWidget {
     return Column(
       children: [
         TheGreatGatsby_pic(),
-        Text(bookBox.get('K_TheGreatGatsby')!.genre,
+        Text(bookBox.get('K_TheGreatGatsby')!.genre + " - " + bookBox.get('K_TheGreatGatsby')!.price.toString() + "\$",
             style: GoogleFonts.oswald(
               color: const Color.fromARGB(255, 45, 41, 41),
               fontSize: 12,
             )),
-        ElevatedButton(
-          onPressed: () {},
-          child: Text(
-            'More information',
-            style: TextStyle(color: Colors.black), // Font color changed to black
-          ),
-          style: ElevatedButton.styleFrom(
-            textStyle: TextStyle(fontSize: 10),
-            backgroundColor: Colors.grey, // Grayish color
-            padding: EdgeInsets.symmetric(vertical: 16, horizontal: 16), // Smaller button padding
-            minimumSize: Size(0, 0), // Smaller button size
-          ),
-        ),
+         Row(
+  mainAxisAlignment: MainAxisAlignment.center, // Center the buttons
+  children: [
+     Row(
+  mainAxisAlignment: MainAxisAlignment.center, // Center the buttons
+  children: [
+    ElevatedButton(
+      onPressed: () {},
+      child: Text(
+        'More',
+        style: TextStyle(color: Colors.black), // Font color changed to black
+      ),
+      style: ElevatedButton.styleFrom(
+        textStyle: TextStyle(fontSize: 10),
+        backgroundColor: Color.fromARGB(255, 124, 142, 197), // Grayish color
+        padding: EdgeInsets.symmetric(vertical: 15, horizontal: 25), // Smaller button padding
+        minimumSize: Size(0, 0),
+      ),
+    ), 
+    SizedBox(width: 8), // Add some spacing between the buttons
+    ElevatedButton(
+      onPressed: () {},
+      child: Text(
+        'Buy',
+        style: TextStyle(color: Colors.black), // Font color changed to black
+      ),
+      style: ElevatedButton.styleFrom(
+        textStyle: TextStyle(fontSize: 10),
+        backgroundColor: Color.fromARGB(255, 124, 142, 197), // Grayish color
+        padding: EdgeInsets.symmetric(vertical: 15, horizontal: 25), // Smaller button padding
+        minimumSize: Size(0, 0),
+      ),
+    ),
+  ],
+),
+
+    
+  ],
+),
       ],
     );
   }
@@ -131,24 +214,43 @@ class AliceInWonderland extends StatelessWidget {
     return Column(
       children: [
         AliceInWonderland_pic(),
-        Text(bookBox.get('K_AliceInWonderland')!.genre,
+        Text(bookBox.get('K_AliceInWonderland')!.genre + " - " + bookBox.get('K_AliceInWonderland')!.price.toString() + "\$",
             style: GoogleFonts.oswald(
               color: const Color.fromARGB(255, 45, 41, 41),
               fontSize: 12,
             )),
-        ElevatedButton(
-          onPressed: () {},
-          child: Text(
-            'More information',
-            style: TextStyle(color: Colors.black), // Font color changed to black
-          ),
-          style: ElevatedButton.styleFrom(
-            textStyle: TextStyle(fontSize: 10),
-            backgroundColor: Colors.grey, // Grayish color
-            padding: EdgeInsets.symmetric(vertical: 16, horizontal: 16), // Smaller button padding
-            minimumSize: Size(0, 0), // Smaller button size
-          ),
-        ),
+        Row(
+  mainAxisAlignment: MainAxisAlignment.center, // Center the buttons
+  children: [
+    ElevatedButton(
+      onPressed: () {},
+      child: Text(
+        'More',
+        style: TextStyle(color: Colors.black), // Font color changed to black
+      ),
+      style: ElevatedButton.styleFrom(
+        textStyle: TextStyle(fontSize: 10),
+        backgroundColor: Color.fromARGB(255, 124, 142, 197), // Grayish color
+        padding: EdgeInsets.symmetric(vertical: 15, horizontal: 25), // Smaller button padding
+        minimumSize: Size(0, 0),
+      ),
+    ), 
+    SizedBox(width: 8), // Add some spacing between the buttons
+    ElevatedButton(
+      onPressed: () {},
+      child: Text(
+        'Buy',
+        style: TextStyle(color: Colors.black), // Font color changed to black
+      ),
+      style: ElevatedButton.styleFrom(
+        textStyle: TextStyle(fontSize: 10),
+        backgroundColor: Color.fromARGB(255, 124, 142, 197), // Grayish color
+        padding: EdgeInsets.symmetric(vertical: 15, horizontal: 25), // Smaller button padding
+        minimumSize: Size(0, 0),
+      ),
+    ),
+  ],
+),
       ],
     );
   }
@@ -161,24 +263,46 @@ class WarAndPeace extends StatelessWidget {
     return Column(
       children: [
         WarAndPeace_pic(),
-        Text(bookBox.get('K_WarAndPeace')!.genre,
-            style: GoogleFonts.oswald(
-              color: const Color.fromARGB(255, 45, 41, 41),
-              fontSize: 12,
-            )),
-        ElevatedButton(
-          onPressed: () {},
-          child: Text(
-            'More information',
-            style: TextStyle(color: Colors.black), // Font color changed to black
-          ),
-          style: ElevatedButton.styleFrom(
-            textStyle: TextStyle(fontSize: 10),
-            backgroundColor: Colors.grey, // Grayish color
-            padding: EdgeInsets.symmetric(vertical: 16, horizontal: 16), // Smaller button padding
-            minimumSize: Size(2, 2), 
-          ),
-        ),
+        Text(bookBox.get('K_WarAndPeace')!.genre + " - " + bookBox.get('K_WarAndPeace')!.price.toString() + "\$",
+          style: GoogleFonts.oswald(
+          color: const Color.fromARGB(255, 45, 41, 41),
+          fontSize: 13,
+  ),
+),
+
+        Row(
+  mainAxisAlignment: MainAxisAlignment.center, // Center the buttons
+  children: [
+    ElevatedButton(
+      onPressed: () {},
+      child: Text(
+        'More',
+        style: TextStyle(color: Colors.black), // Font color changed to black
+      ),
+      style: ElevatedButton.styleFrom(
+        textStyle: TextStyle(fontSize: 10),
+        backgroundColor: Color.fromARGB(255, 124, 142, 197), // Grayish color
+        padding: EdgeInsets.symmetric(vertical: 15, horizontal: 25), // Smaller button padding
+        minimumSize: Size(0, 0),
+      ),
+    ), 
+    SizedBox(width: 8), // Add some spacing between the buttons
+    ElevatedButton(
+      onPressed: () {},
+      child: Text(
+        'Buy',
+        style: TextStyle(color: Colors.black), // Font color changed to black
+      ),
+      style: ElevatedButton.styleFrom(
+        textStyle: TextStyle(fontSize: 10),
+        backgroundColor: Color.fromARGB(255, 124, 142, 197), // Grayish color
+        padding: EdgeInsets.symmetric(vertical: 15, horizontal: 25), // Smaller button padding
+        minimumSize: Size(0, 0),
+      ),
+    ),
+  ],
+),
+
       ],
     );
   }
